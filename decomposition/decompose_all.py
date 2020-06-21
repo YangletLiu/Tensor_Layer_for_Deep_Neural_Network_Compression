5ff4950a31bef912cbac8dbd9757d6d250fd05f8
Yiming Fang <yf2484@columbia.edu>
2020-06-22 00:51:34 +0800

    restructure

---
+++ decomposition/decompose_all.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import models
+import tensorly as tl
+import tensorly
+from itertools import chain
+
+from conv_layer import *
+
+# decompose all layers in a network
+def decompose_all(decomp):
+    model = torch.load("models/model").cuda()
+    model.eval()
+    model.cpu()
+    for i, key in enumerate(model.features._modules.keys()):
+        if i >= len(model.features._modules.keys()) - 2:
+            break
+        conv_layer = model.features._modules[key]
+        if isinstance(conv_layer, torch.nn.modules.conv.Conv2d):
+            rank = max(conv_layer.weight.data.numpy().shape) // 10
+            if decomp == 'cp':
+                model.features._modules[key] = cp_decomposition_conv_layer(conv_layer, rank)
+            if decomp == 'tucker': 
+                ranks = [int(np.ceil(conv_layer.weight.data.numpy().shape[0] / 3)), 
+                         int(np.ceil(conv_layer.weight.data.numpy().shape[1] / 3))]
+                model.features._modules[key] = tucker_decomposition_conv_layer(conv_layer, ranks)
+            if decomp == 'tt':
+                model.features._modules[key] = tt_decomposition_conv_layer(conv_layer, rank)
+        torch.save(model, 'models/model')
+    return model
---
+++ nets/lenet.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from torch.optim.lr_scheduler import StepLR
+
+import torchvision
+import torchvision.transforms as transforms
+from torchvision import models
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 32, 3, 1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 64, 3, 1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.25),
+            nn.Linear(9216, 128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(128, 10),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        output = F.log_softmax(x, dim=1)
+        return output
